448f24422793f8a9098f22291ab6d1150726257d
Swetha Thomas <78926897+swetha-thomas@users.noreply.github.com>
2021-11-27 09:56:27 +0400

    Program 7

---
+++ Assignment 1 (Graphs)/RedundantConnection.py
@@ -0,0 +1,17 @@
+class Solution:
+    def findRedundantConnection(self, edges: List[List[int]]) -> List[int]:
+      parent = [i for i in range(len(edges) + 1)]
+
+      def find(x): # Finding the root parent of x
+          if x != parent[x]:
+              parent[x] = find(parent[x])
+          return parent[x]
+
+      def union(x, y):
+          parent[find(y)] = find(x)   # Make the root parent of y = root parent of x (Single component)
+
+      for a, b in edges:
+          if find(a) == find(b):  # If a and b have same root parent, then path between a and b is redundant
+              return [a, b]
+          else:
+              union(a, b)
